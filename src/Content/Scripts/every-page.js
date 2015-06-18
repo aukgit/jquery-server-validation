e1e4405f3d4e18d68deb08a003eedf905b1c90a8
Alim Ul Karim <devorg.bd@gmail.com>
2015-06-18 18:43:12 +0600

    initial push

---
+++ src/Content/Scripts/every-page.js
@@ -0,0 +1,58 @@
+﻿/*!
+ * Written by Alim Ul Karim
+ * http://alimkarim.com
+ * me{at}alimkarim.com
+*/
+
+$.genericPage = function() {
+    function transactionStatusHide() {
+        var $transactionStatus = $(".transaction-status");
+        if ($transactionStatus.length > 0) {
+            $transactionStatus.delay(1500).fadeOut(2500);
+        }
+    }
+    var $tooltipItems = $('.tooltip-show');
+    if ($tooltipItems.length > 0) {
+        $tooltipItems.tooltip();
+    }
+    var $seoHideItems = $(".seo-hide");
+    if ($seoHideItems.length > 0) {
+        $seoHideItems.hide();
+    }
+    transactionStatusHide();
+
+    $("div.datetimepicker-start").datetimepicker({
+        pickDate: true, //en/disables the date picker
+        pickTime: true, //en/disables the time picker
+        useMinutes: true, //en/disables the minutes picker
+        useSeconds: true, //en/disables the seconds picker
+        useCurrent: true, //when true, picker will set the value to the current date/time     
+        minuteStepping: 1, //set the minute stepping
+        defaultDate: "", //sets a default date, accepts js dates, strings and moment objects
+        disabledDates: [], //an array of dates that cannot be selected
+        enabledDates: [], //an array of dates that can be selected
+        sideBySide: true //show the date and time picker side by side
+
+    });
+
+    $("div.datepicker-start").datetimepicker({
+        pickDate: true, //en/disables the date picker
+        pickTime: false, //en/disables the time picker
+        useMinutes: false, //en/disables the minutes picker
+        useSeconds: false, //en/disables the seconds picker
+        useCurrent: true, //when true, picker will set the value to the current date/time     
+        minuteStepping: 1, //set the minute stepping
+        defaultDate: "", //sets a default date, accepts js dates, strings and moment objects
+        disabledDates: [], //an array of dates that cannot be selected
+        enabledDates: [], //an array of dates that can be selected
+
+        sideBySide: true //show the date and time picker side by side
+
+    });
+}
+
+
+$(function () {
+
+    $.genericPage();
+});
